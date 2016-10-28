6b89f2910969d03a0199b2d68a9c3b12133cdb83
Aditya Gupta <adityagupta1089@gmail.com>
2016-10-28 15:08:13 +0530

    .

--- src/SimpleRISCEncoder.java
+++ src/SimpleRISCEncoder.java
@@ -1,129 +1,201 @@
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
-import java.util.ArrayList;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Scanner;
+import java.util.TreeMap;
 import java.util.stream.IntStream;
 
 public class SimpleRISCEncoder {
 
-	public static final String[] op1 = { "add", "sub", "mul", "div", "mod", "cmp", "and", "or", "not", "mov", "lsl",
-			"lsr", "asr", "nop", "ld", "st", "beq", "bgt", "b", "call", "ret" };
-	public static final String[] zai = { "ret", "nop" };
-	public static final String[] oai = { "call", "b", "beq", "bgt" };
-	public static final String[] tai = { "add", "sub", "mul", "div", "mod", "and", "or", "lsl", "lsr", "asr" };
+	public static final String[] operands = { "add", "sub", "mul", "div", "mod", "cmp", "and", "or", "not", "mov",
+			"lsl", "lsr", "asr", "nop", "ld", "st", "beq", "bgt", "b", "call", "ret" };
+	public static final String[] zeroArgumentOperands = { "ret", "nop" };
+	public static final String[] oneArgumentOperands = { "call", "b", "beq", "bgt" };
+	public static final String[] twoArgumentOperands = { "add", "sub", "mul", "div", "mod", "and", "or", "lsl", "lsr",
+			"asr" };
 
-	public static void main(String[] args) throws Exception {
-		Map<String, Integer> op2 = new HashMap<>();
-		IntStream.range(0, op1.length).forEach(i -> op2.put(op1[i], i));
-		Scanner sc = new Scanner(new FileReader("input.txt"));
-		List<String> instructions = new ArrayList<>();
-		FileWriter fw = new FileWriter("/home/aditya/workspace/C++/CSL211/simple RISC emulator/test/test.mem");
-		while (sc.hasNext()) {
-			instructions.add(sc.nextLine());
+	public static void main(String[] args) {
+		// =================================================
+		// Operand OPCode
+		// =================================================
+		Map<String, Integer> operandOpcodeMap = new HashMap<>();
+		IntStream.range(0, operands.length).forEach(i -> operandOpcodeMap.put(operands[i], i));
+		// =================================================
+		// IO
+		// =================================================
+		Scanner scanner = null;
+		try {
+			scanner = new Scanner(new FileReader(args[0]));
+		} catch (FileNotFoundException e) {
+			System.out.println("Input File Not Found");
 		}
-		int pc = 0;
-		for (int i = 0; i < instructions.size(); i++) {
-			String line = instructions.get(i);
-			if (line.contains(":")) {
-				System.out.println(line);
-				continue;
+		FileWriter fileWriter = null;
+		try {
+			fileWriter = new FileWriter(args[1]);
+		} catch (IOException e) {
+			System.out.println("Output File Not Found");
+		}
+		// =================================================
+		// Maps
+		// =================================================
+		Map<String, Integer> labelPCMap = new HashMap<>();
+		Map<Integer, String> reverselabelPCMap = new HashMap<>();
+		TreeMap<Integer, String> nonLabelStatementsPCMap = new TreeMap<>();
+		// =================================================
+		int PC = 0;
+		int maxLineLength = 0;
+		while (scanner.hasNext()) {
+			String line = scanner.nextLine();
+			line = line.replaceFirst("@.+", "").trim();
+			if (line.length() > 0) {
+				maxLineLength = Math.max(maxLineLength, line.length());
+				if (line.contains(":")) {
+					labelPCMap.put(line.substring(0, line.indexOf(':')), PC);
+					reverselabelPCMap.put(PC, line.substring(0, line.indexOf(':')));
+				} else {
+					nonLabelStatementsPCMap.put(PC, line);
+					PC += 4;
+				}
 			}
+		}
+		maxLineLength++;
+		// =================================================
+		for (Entry<Integer, String> statement : nonLabelStatementsPCMap.entrySet()) {
+			// =================================================
+			int currentPC = statement.getKey();
+			String line = statement.getValue();
 			long instruction = 0;
+			// =================================================
 			String[] words = Arrays.stream(line.split("[\\s,\\[\\]]+")).filter(x -> x.length() > 0)
 					.toArray(size -> new String[size]);
-			char lc = words[0].charAt(words[0].length() - 1);
-			if (lc == 'h')
+			// =================================================
+			char lastCharacterOperand = words[0].charAt(words[0].length() - 1);
+			// Modifier Bits
+			if (lastCharacterOperand == 'h')
 				instruction += 1 << 17;
-			if (lc == 'u')
+			if (lastCharacterOperand == 'u')
 				instruction += 1 << 16;
-			long opcode = op2.get(words[0]);
+			// =================================================
+			long opcode = -1;
+			if (operandOpcodeMap.containsKey(words[0]))
+				opcode = operandOpcodeMap.get(words[0]);
+			else
+				System.out.println("Invalid operand \"" + words[0] + "\".");
 			instruction += opcode << 27;
-			if (Arrays.stream(zai).anyMatch(x -> x.equals(words[0]))) {
-
-			} else if (Arrays.stream(oai).anyMatch(x -> x.equals(words[0]))) {
-				int off = 0;
-				for (int j = 0; j < instructions.size(); j++) {
-					if (instructions.get(j).contains(words[1] + ":")) {
-						for (int x = (j > i) ? i + 1 : i - 1; (j > i) ? x <= j : x >= j; x += (j > i) ? 1 : -1) {
-							for (String st : op1)
-								if (instructions.get(x).contains(st) && !instructions.get(x).contains(":")) {
-									off += (j > i) ? 1 : -1;
-									break;
-								}
-						}
-						if (j > i)
-							off++;
-						break;
-					}
-				}
-				String offs = Integer.toBinaryString(off);
-				if (off < 0)
-					offs = offs.substring(offs.length() - 27);
-				instruction += Integer.parseInt(offs, 2);
-			} else if (Arrays.stream(tai).anyMatch(x -> x.equals(words[0]))) {
+			// =================================================
+			if (Arrays.stream(zeroArgumentOperands).anyMatch(x -> x.equals(words[0]))) {
+				// Nothing To Do
+			} else if (Arrays.stream(oneArgumentOperands).anyMatch(x -> x.equals(words[0]))) {
+				int offset = 0;
+				if (labelPCMap.containsKey(words[1])) {
+					// =================================================
+					offset = labelPCMap.get(words[1]) - currentPC;
+					String offsetString = Integer.toBinaryString(offset);
+					if (offset < 0)
+						offsetString = offsetString.substring(offsetString.length() - 27);
+					instruction += Integer.parseInt(offsetString, 2);
+					// =================================================
+				} else
+					System.out.println("Label \"" + words[1] + "\" Not Found");
+			} else if (Arrays.stream(twoArgumentOperands).anyMatch(x -> x.equals(words[0]))) {
+				// =================================================
 				int I = words[3].contains("r") ? 0 : 1;
 				instruction += I << 26;
-				int rd = reg(words[1]);
+				// =================================================
+				int rd = registerNumber(words[1]);
 				instruction += rd << 22;
-				int rs1 = reg(words[2]);
+				// =================================================
+				int rs1 = registerNumber(words[2]);
 				instruction += rs1 << 18;
+				// =================================================
 				if (I == 0) {
-					int rs2 = reg(words[3]);
+					int rs2 = registerNumber(words[3]);
 					instruction += rs2 << 14;
 				} else {
 					instruction += Integer.parseInt(words[3]);
 				}
+				// =================================================
 			} else if (words[0].equals("cmp") || words[0].equals("not") || words[0].equals("mov")) {
+				// =================================================
 				int I = words[2].contains("r") ? 0 : 1;
 				instruction += I << 26;
-				int rx = reg(words[1]);
+				// =================================================
+				int rx = registerNumber(words[1]);
 				if (words[0].equals("cmp")) {
 					instruction += rx << 18;
 				} else {
 					instruction += rx << 22;
 				}
+				// =================================================
 				if (I == 0) {
-					int rs2 = reg(words[2]);
+					int rs2 = registerNumber(words[2]);
 					instruction += rs2 << 14;
 				} else {
 					instruction += Integer.parseInt(words[2]);
 				}
-			} else {
+				// =================================================
+			} else /* ld, st */ {
+				// =================================================
 				int I = words[2].contains("r") ? 0 : 1;
 				instruction += I << 26;
-				int rd = reg(words[1]);
+				// =================================================
+				int rd = registerNumber(words[1]);
 				instruction += rd << 22;
-				int rs1 = reg(words[3]);
+				// =================================================
+				int rs1 = registerNumber(words[3]);
 				instruction += rs1 << 18;
+				// =================================================
 				if (I == 0) {
-					int rs2 = reg(words[2]);
+					int rs2 = registerNumber(words[2]);
 					instruction += rs2 << 14;
 				} else {
 					instruction += Integer.parseInt(words[2]);
 				}
+				// =================================================
 			}
-			String ins = Long.toHexString(instruction).toUpperCase();
-			while (ins.length() < 8)
-				ins = "0" + ins;
-			fw.write("0x" + Integer.toHexString(pc).toUpperCase() + " 0x" + ins + "\n");
-			System.out.println(pc + ":" + line + " -> 0x" + Integer.toHexString(pc).toUpperCase() + " 0x" + ins);
-			pc += 4;
+			// =================================================
+			try {
+				fileWriter.write(String.format("0x%02X 0x%08X\n", currentPC, instruction));
+			} catch (IOException error) {
+				error.printStackTrace();
+			}
+			// =================================================
+			System.out.printf("[%03d] : [\"%-" + maxLineLength + "s] [0x%02X 0x%08X]\n", currentPC, line + "\"",
+					currentPC, instruction);
+			if (reverselabelPCMap.containsKey(currentPC + 4)) {
+				System.out.printf("[   ] : [\"%-" + maxLineLength + "s]\n",
+						reverselabelPCMap.get(currentPC + 4) + "\"");
+			}
+			// =================================================
 		}
-		sc.close();
-		fw.close();
+		// =================================================
+		scanner.close();
+		try {
+			fileWriter.close();
+		} catch (IOException error) {
+			error.printStackTrace();
+		}
+		// =================================================
 	}
 
-	private static int reg(String rx) {
+	private static int registerNumber(String rx) {
+		// =================================================
 		if (rx.equals("sp"))
 			return 14;
+		// =================================================
 		else if (rx.equals("ra"))
 			return 15;
+		// =================================================
 		if (rx.contains("r"))
 			return Integer.parseInt(rx.substring(1));
-		throw new RuntimeException();
+		// =================================================
+		System.out.println("Such a register not found");
+		return -1;
 	}
+
 }
